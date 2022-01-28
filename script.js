37f6ddd415f5c07115f9792fe21de05806035076
Cody <74194707+lewisoutdoorllc@users.noreply.github.com>
2022-01-27 21:51:04 -0500

    updated js file

---
+++ script.js
@@ -0,0 +1,29 @@
+// console.log('hello world');
+
+// let newNFT = {
+//     bgColor: 'red',
+//     name: 'DOG',
+//     owner: 'Cody',
+// }
+
+// document.body.style.backgroundColor = newNFT.bgColor;
+
+// document.body.style.backgroundColor = 'red';
+
+// document.name.backgroundColor = 'red';
+
+// let dog = {
+//     name: "Shibe #5079",
+//     price: "0.09ETH",
+//     description: "Shiba Shelter is a collection of shibes on the Ethereum Blockchain",
+//     nft: "https://lh3.googleusercontent.com/BLAw9VE-D5PyYfu7u_KbxBIrl_j5jI8I4UAt8H-qTGRZ4FBaFWNKb0HHP5Uk1shKvmC5LGpfi1ivK8G2BDuZOE30TVS8J8W-WngOyIE=w600",
+//     backgroundColor: "#00FFFF",
+//     cardColor: "#FFC579",
+//     otherData: {
+//         owner: "Lance Toledo",
+//         daysLeft: "1 day left",
+//         profileImg: "https://lh3.googleusercontent.com/a-/AOh14GiBv7fSEkk1Kr_3jpQfHTuEHh6V1ehFsg_sLNg0=s40-c",
+//     }
+// }
+
+
